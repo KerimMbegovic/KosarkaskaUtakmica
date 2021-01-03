886a992f78573cc662430c7eeabc49c01f5faab3
Kerim Muharembegović <57753259+KerimMbegovic@users.noreply.github.com>
2021-01-03 12:08:58 +0100

    Add files via upload

---
+++ Klub.cpp
@@ -0,0 +1,54 @@
+#include "Klub.h"
+#include <iostream>
+using std::cin;
+using std::cout;
+Klub::Klub()
+{
+}
+
+void Klub::setNaziv()
+{
+    cout << "Unesite naziv kluba: ";
+    cin.getline(this->naziv, 30);
+}
+
+void Klub::setIgraci()
+{
+    Igrac temp;
+    for (int i = 0; i < 5; i++) {
+        cout << "Igrac br. " << i + 1 << ":\n";
+        cin >> temp;
+        this->igraci.push_back(temp);
+    }
+}
+
+char* Klub::getNaziv()
+{
+    return this->naziv;
+}
+
+std::vector<Igrac>& Klub::getIgraci()
+{
+    return this->igraci;
+}
+
+std::istream& operator>>(std::istream& stream, Klub& k)
+{
+    k.setNaziv();
+    k.setIgraci();
+    return stream;
+}
+
+std::ostream& operator<<(std::ostream& stream, Klub& k)
+{
+    stream << k.getNaziv() << "\n";
+    for (int i = 0; i < k.getIgraci().size(); i++) {
+        stream << "\t" << k.getIgraci()[i] << "\n";
+    }
+    return stream;
+}
+
+Klub::~Klub()
+{
+}
+
---
+++ main.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <ctime>
+#include <stdlib.h>
+#include "Windows.h"
+#include "Utakmica.h"
+using namespace std;
+
+int main() {
+	Utakmica u;
+	cin >> u;
+	int x, prviNapad;
+	
+	x = rand() % 2;
+	prviNapad = x;
+	system("cls");
+	cout << "Prva cetvrtina:\n";
+	Sleep(1000);
+	for (int i = 0; i < 20; i++) {
+		if (x == 0) {
+			++u;
+			x++;
+			Sleep(500);
+		}
+		else {
+			u++;
+			x--;
+			Sleep(500);
+		}
+	}
+	cout << u;
+	system("pause");
+	system("cls");
+
+	x = (prviNapad + 1) % 2;
+
+	cout << "Druga cetvrtina:\n";
+	Sleep(1000);
+	for (int i = 0; i < 20; i++) {
+		if (x == 0) {
+			++u;
+			x++;
+			Sleep(500);
+		}
+		else {
+			u++;
+			x--;
+			Sleep(500);
+		}
+	}
+	cout << u;
+	system("pause");
+	system("cls");
+	
+	x = (prviNapad + 1) % 2;
+
+	cout << "Treca cetvrtina:\n";
+	Sleep(1000);
+	for (int i = 0; i < 20; i++) {
+		if (x == 0) {
+			++u;
+			x++;
+			Sleep(500);
+		}
+		else {
+			u++;
+			x--;
+			Sleep(500);
+		}
+	}
+	cout << u;
+	system("pause");
+	system("cls");
+
+	cout << "Cetvrta cetvrtina:\n";
+	x = prviNapad;
+	Sleep(1000);
+	for (int i = 0; i < 20; i++) {
+		if (x == 0) {
+			++u;
+			x++;
+			Sleep(500);
+		}
+		else {
+			u++;
+			x--;
+			Sleep(500);
+		}
+	}
+	cout << u;
+	system("pause");
+	system("cls");
+
+	while (u.provjera()) {
+		x = rand() % 2;
+		cout << "Produzeci:\n";
+		Sleep(1000);
+		for (int i = 0; i < 10; i++) {
+			if (x == 0) {
+				++u;
+				x++;
+				Sleep(500);
+			}
+			else {
+				u++;
+				x--;
+				Sleep(500);
+			}
+		}
+		cout << u;
+		system("pause");
+		system("cls");
+	}
+
+	!u;
+	return 0;
+}
